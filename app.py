a54014d68e4a5d7da793f60b27a85dd34497483d
thenameisnat <talbot.nat@gmail.com>
2019-04-05 20:21:33 -0400

    All initial files up to database migration setup
    Database now had one table ClubMember and migrates successfully with this

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+import models
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,43 @@
+from app import db
+
+'''
+Explanation:
+This is a 'pythonic' way of representing database objects in postgres
+in lieu of using raw SQL statements in python code
+This allows for extensibility as well as for generally cleaner-looking
+code. 
+Documentation can be found here: https://docs.sqlalchemy.org/en/latest/orm/tutorial.html
+'''
+class ClubMember(db.Model):
+    __tablename__ = 'ClubMember'
+
+    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
+    status = db.Column(db.Boolean())
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+
+    def __init__(self, status, name, email):
+        self.status = status
+        self.name = name
+        self.email = email
+
+    # overriding default __repr__ method 
+    def __repr__(self):
+        return 'id:{}'.format(self.id)
+    
+    # overriding default __str__ method for object which allows printing
+    def __str__(self):
+        return 'id: {}\nstatus: {}\nname: {}\nemail: {}'.format(self.id, self.status, self.name, self.email)
+
+    #potentially useful for JSON usage
+    def serialize(self):
+        return {
+            'id': self.id,
+            'status': self.status,
+            'name': self.name,
+            'email': self.email
+        }
+'''
+class Reservations(db.Model):
+    member = db.Column(Integer
+'''
