a8fe415109c335a116751149b474be4ab0a584d1
thenameisnat <talbot.nat@gmail.com>
2019-04-06 01:06:35 -0400

    Initial working draft.
    `Has HTML template and working add member form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,11 +7,30 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-import models
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from models import club_member
+
+#For page for adding members.
+@app.route("/add_member", methods=['GET', 'POST'])
+def add_member():
+    if request.method == 'POST':
+        status = request.form.get('status')
+        if (status == "True"):
+            status = True
+        else:
+            status = False
+        name = request.form.get('name')
+        email = request.form.get('email')
+        try:
+            member = club_member(status=status, name=name, email=email)
+            db.session.add(member)
+            db.session.commit()
+            return "Member added. member id={}".format(member.id)
+        except Exception as e:
+            return(str(e))
+    return render_template("add_member.html")         
+
+    
 
 if __name__ == '__main__':
     app.run()
