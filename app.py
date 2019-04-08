35947deece9aa803579146a23c6476e680921545
thenameisnat <talbot.nat@gmail.com>
2019-04-08 11:44:31 -0400

    Added gear page

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import club_member
+from models import club_member, gear_item
 
 #For page for adding members.
 @app.route("/add_member", methods=['GET', 'POST'])
@@ -28,9 +28,28 @@ def add_member():
             return "Member added. member id={}".format(member.id)
         except Exception as e:
             return(str(e))
-    return render_template("add_member.html")         
+    return render_template("add_member.html")
 
-    
-
+#page for adding gear
+@app.route("/add_gear", methods=['GET', 'POST'])
+def add_gear():
+    if request.method == 'POST':
+        status = request.form.get('status')
+        if (status == "True"):
+            status = True
+        else:
+            status = False
+        name = request.form.get("name")
+        brand = request.form.get("brand")
+        condition = request.form.get("condition")
+        best_use = request.form.get("best_use")
+        try:
+            item = gear_item(status=status, brand=brand, name=name, condition=condition, best_use=best_use)
+            db.session.add(item)
+            db.session.commit()
+            return "Gear added. gear id={}".format(item.gear_id)
+        except Exception as e:
+            return(str(e))
+    return render_template("add_gear.html")
 if __name__ == '__main__':
     app.run()
