5334d3c69f7b76a84a05d6d95a8a13b510bc10d9
thenameisnat <talbot.nat@gmail.com>
2019-04-19 21:38:47 -0400

    hopefully fixing migration issues`

--- migrations/versions/84634bfb47eb_.py
+++ migrations/versions/84634bfb47eb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 5466cf9b41a8
+Revision ID: 84634bfb47eb
 Revises: 
-Create Date: 2019-04-19 21:29:30.232825
+Create Date: 2019-04-19 21:38:33.527147
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5466cf9b41a8'
+revision = '84634bfb47eb'
 down_revision = None
 branch_labels = None
 depends_on = None
